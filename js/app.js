7e9b1aa6f2250cf70f210428ba9a810d607a8c95
Nghia Dang <nghia12a319@gmail.com>
2021-10-09 17:29:36 +0700

    js for navbar and responsive About for mobile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
-
-
 const navSlide = function (){
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_content-list');
     const navLinks = document.querySelectorAll('.nav_content-item')
+    const menuItems = document.querySelectorAll('#nav li a[href*="#"]')
 
-    
+    console.log(menuItems);
+
     burger.addEventListener('click',()=>{
         //toggle nav
         nav.classList.toggle('nav-active');
+        
+        // burger animation
+        burger.classList.toggle('toggle');
 
         //Animation item
         navLinks.forEach((link,index)=>{
@@ -16,15 +19,26 @@ const navSlide = function (){
                 link.style.animation = ''
             }else{
                 link.style.animation = `navItemFade ease forwards 0.5s  ${index / 7 + 0.5}s`;
-                console.log(link.style.animation)
             }
+            console.log(link.style.animation)
         });
-
-        // burger animation
-        burger.classList.toggle('toggle');
     });
 
+    // đóng menu khi bấm vào item, chưa đóng khi bấm ra ngoài nav
+    nav.onclick = function(e){
+        if (e.target.id !== 'nav' && e.target.id !== 'burger') {
+            nav.classList.remove('nav-active');
+            burger.classList.remove('toggle');
+        }
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation = ''
+            }else{
+                link.style.animation = `navItemFade ease forwards 0.5s  ${index / 7 + 0.5}s`;
+            }
+            console.log(link.style.animation)
+        });
+    }
 
 }
-
 navSlide();
