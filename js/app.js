816451404645ac6976213fd5f4ce02d2bf219dfb
nghiadang-vizion <108381566+nghiadang-vizion@users.noreply.github.com>
2025-02-10 13:36:05 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,71 +1,82 @@
+const navSlide = function () {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav_content-list");
+  const navLinks = document.querySelectorAll(".nav_content-item");
+  const menuItems = document.querySelectorAll('#nav li a[href*="#"]');
 
+  console.log(menuItems);
 
-const navSlide = function (){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_content-list');
-    const navLinks = document.querySelectorAll('.nav_content-item')
-    const menuItems = document.querySelectorAll('#nav li a[href*="#"]')
+  burger.addEventListener("click", () => {
+    //toggle nav
+    nav.classList.toggle("nav-active");
 
-    console.log(menuItems);
+    // burger animation
+    burger.classList.toggle("toggle");
 
-    burger.addEventListener('click',()=>{
-        //toggle nav
-        nav.classList.toggle('nav-active');
-        
-        // burger animation
-        burger.classList.toggle('toggle');
-
-        //Animation item
-        navLinks.forEach((link,index)=>{
-            if(link.style.animation){
-                link.style.animation = ''
-            }else{
-                link.style.animation = `navItemFade ease forwards 0.5s  ${index / 7 + 0.5}s`;
-            }
-            console.log(link.style.animation)
-        });
+    //Animation item
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navItemFade ease forwards 0.5s  ${
+          index / 7 + 0.5
+        }s`;
+      }
+      console.log(link.style.animation);
     });
+  });
 
-    // đóng menu khi bấm vào item, chưa đóng khi bấm ra ngoài nav
-    nav.onclick = function(e){
-        if (e.target.id !== 'nav' && e.target.id !== 'burger') {
-            nav.classList.remove('nav-active');
-            burger.classList.remove('toggle');
-        }
-        navLinks.forEach((link,index)=>{
-            if(link.style.animation){
-                link.style.animation = ''
-            }else{
-                link.style.animation = `navItemFade ease forwards 0.5s  ${index / 7 + 0.5}s`;
-            }
-            console.log(link.style.animation)
-        });
+  // đóng menu khi bấm vào item, chưa đóng khi bấm ra ngoài nav
+  nav.onclick = function (e) {
+    if (e.target.id !== "nav" && e.target.id !== "burger") {
+      nav.classList.remove("nav-active");
+      burger.classList.remove("toggle");
     }
-
-}
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navItemFade ease forwards 0.5s  ${
+          index / 7 + 0.5
+        }s`;
+      }
+      console.log(link.style.animation);
+    });
+  };
+};
 navSlide();
 
 var lasstScrollTop = 0;
-const navbar = document.querySelector('.header')
-var srollTopBtn = document.querySelector('.social-icons')
+const navbar = document.querySelector(".header");
+var srollTopBtn = document.querySelector(".social-icons");
 window.addEventListener("scroll", function () {
-    //ẩn hiện header
-    var srollTop = this.window.pageYOffset || this.document.documentElement.scrollTop;
-    
-    if (srollTop > lasstScrollTop) {
-        navbar.style.top = '-133px';
-    }else{
-        navbar.style.top = '0';
-    }
-    lasstScrollTop = srollTop;
+  //ẩn hiện header
+  var srollTop =
+    this.window.pageYOffset || this.document.documentElement.scrollTop;
 
-    //ẩn hiện btn sroll top
-    srollTopBtn.classList.toggle("active", this.window.scrollY > 800);
-})
+  if (srollTop > lasstScrollTop) {
+    navbar.style.top = "-133px";
+  } else {
+    navbar.style.top = "0";
+  }
+  lasstScrollTop = srollTop;
+
+  //ẩn hiện btn sroll top
+  srollTopBtn.classList.toggle("active", this.window.scrollY > 800);
+});
 
 srollTopBtn.onclick = function () {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-}
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+const cvBtn = document.querySelector("#cv_btn");
+console.log("🚀 ~ cvBtn:", cvBtn);
+cvBtn.onclick = function (e) {
+  return window.open(
+    "https://www.topcv.vn/xem-cv/UlxTV1xUVgAHUABdUlRdAQhWBgtaAlJSVVRQVge580",
+    "_blank"
+  );
+};
